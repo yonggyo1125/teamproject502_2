3db656d1f2e81c986f8724b5c7fb4275da85bd86
yonggyo1125 <yonggyo00@kakao.com>
2024-08-30 12:07:47 +0900

    .

--- react_project_P-4-master/src/mypage/component/ProfileForm.js
+++ react_project_P-4-master/src/mypage/component/ProfileForm.js
@@ -3,15 +3,41 @@ import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 import InputBox from '../../commons/components/InputBox';
 
-const FormBox = styled.form`
+const FormBox = styled.form``;
 
-`;
+const ProfileForm = ({ form, onChange, onSubmit }) => {
+  const { t } = useTranslation();
 
-const ProfileForm = ({form, onChange, onSubmit }) => {
-
-    return <FormBox onSubmit={onSubmit} autoComplete='off'>
-
+  return (
+    <FormBox onSubmit={onSubmit} autoComplete="off">
+      <dl>
+        <dt>{t('이메일')}</dt>
+        <dd>{form?.email}</dd>
+      </dl>
+      <dl>
+        <dt>{t('회원명')}</dt>
+        <dd>
+          <InputBox
+            type="text"
+            name="userName"
+            value={form?.userName}
+            onChange={onChange}
+          />
+        </dd>
+      </dl>
+      <dl>
+        <dt>{t('비밀번호')}</dt>
+        <dd>
+          <InputBox
+            type="password"
+            name="password"
+            value={form?.password}
+            onChange={onChange}
+          />
+        </dd>
+      </dl>
     </FormBox>
+  );
 };
 
 export default React.memo(ProfileForm);
