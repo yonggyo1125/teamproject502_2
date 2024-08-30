a49c599c279403858bc4fbfd151fe4cfd9737565
yonggyo1125 <yonggyo00@kakao.com>
2024-08-30 19:05:07 +0900

    .

--- react_project_P-4-master/src/restaurant/components/TabReview.js
+++ react_project_P-4-master/src/restaurant/components/TabReview.js
@@ -1,46 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Tab } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
-import { getInfo } from '../../board/apis/apiBoard';
 import styled from 'styled-components';
-
+import Pagination from '../../commons/components/Pagination';
 const NoDataText = styled.div`
   font-size: 16px;
   color: #818181;
 `;
 
-const TabReview = () => {
+const TabReview = ({ items, pagination, onPageClick }) => {
+  console.log(items);
   const { t } = useTranslation();
-  const [boardData, setBoardData] = useState(null);
-  const { seq } = useParams();
-
-  useEffect(() => {
-    const fetchBoardData = async () => {
-      try {
-        const data = await getInfo(seq);
-        setBoardData(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchBoardData();
-  }, [seq]);
-
   return (
     <Tab.Pane eventKey="review">
       <div>
-        {boardData ? (
-          <>
-            <h2>{boardData.subject}</h2>
-            <p>{boardData.content}</p>
-            <p>{boardData.poster}</p>
-            <p>{boardData.viewCount}</p>
-          </>
+        {items && items.length > 0 ? (
+          items.map(({ subject, content, poster, viewCount }) => (
+            <>
+              <h2>{subject}</h2>
+              <p dangerouslySetInnerHTML={{ __html: content }}></p>
+              <p>{poster}</p>
+              <p>{viewCount}</p>
+            </>
+          ))
         ) : (
-          <NoDataText>{t('리뷰 정보가 없습니다')}</NoDataText>
+          <NoDataText>{t('리뷰_정보가_없습니다')}</NoDataText>
         )}
       </div>
+      {pagination && (
+        <Pagination pagination={pagination} onClick={onPageClick} />
+      )}
     </Tab.Pane>
   );
 };
