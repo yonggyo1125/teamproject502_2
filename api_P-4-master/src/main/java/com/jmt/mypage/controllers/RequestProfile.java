670612879088f9239bcf453d83119c319c452c73
yonggyo1125 <yonggyo00@kakao.com>
2024-08-30 14:41:24 +0900

    .

--- api_P-4-master/src/main/java/com/jmt/mypage/controllers/RequestProfile.java
+++ api_P-4-master/src/main/java/com/jmt/mypage/controllers/RequestProfile.java
@@ -1,9 +1,11 @@
 package com.jmt.mypage.controllers;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RequestProfile {
     @NotBlank
     private String userName;
