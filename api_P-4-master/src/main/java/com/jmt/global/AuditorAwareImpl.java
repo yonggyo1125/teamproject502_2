0c4c759fbca78307b356e10878f492fd34c8aabe
yonggyo1125 <yonggyo00@kakao.com>
2024-08-30 15:48:20 +0900

    111

--- api_P-4-master/src/main/java/com/jmt/global/AuditorAwareImpl.java
+++ api_P-4-master/src/main/java/com/jmt/global/AuditorAwareImpl.java
@@ -3,22 +3,22 @@ package com.jmt.global;
 import com.jmt.member.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-@RequiredArgsConstructor
 public class AuditorAwareImpl implements AuditorAware<String> {
-
-    private final MemberUtil memberUtil;
 
     @Override
     public Optional<String> getCurrentAuditor() {
 
         String email = null;
-        if (memberUtil.isLogin()) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails) {
             email = SecurityContextHolder.getContext().getAuthentication().getName();
         }
 
